c358161d2d3d13ca87dff40d501df3ec676bd4b7
Thanh Dao <daokythanh.343@gmail.com>
2018-11-11 23:29:01 +0700

    + product insert component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
 
 const routes: Routes = [
   {
@@ -15,12 +16,16 @@ const routes: Routes = [
       {
         path: 'list',
         component: ProductListComponent
+      },
+      {
+        path: 'insert',
+        component: ProductInsertComponent
       }
     ]
   }
 ];
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductListComponent, ProductInsertComponent],
   imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class ProductModule {}
